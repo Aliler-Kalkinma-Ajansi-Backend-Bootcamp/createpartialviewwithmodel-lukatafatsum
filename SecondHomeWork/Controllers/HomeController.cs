a99220c4eb0da47c9dc4bb1142e0c0869ed660a6
mustafa atakul <mselmanatakul@gmail.com>
2021-01-01 22:37:32 +0300

    some practices

--- SecondHomeWork/Controllers/HomeController.cs
+++ SecondHomeWork/Controllers/HomeController.cs
@@ -11,12 +11,12 @@ namespace SecondHomeWork.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public IActionResult Index()
         {
@@ -27,6 +27,13 @@ namespace SecondHomeWork.Controllers
         {
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ SecondHomeWork/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondHomeWork.Models
+{
+    public class Login
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+
+    }
+}
